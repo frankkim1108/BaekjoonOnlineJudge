aa0555b9862fbaf68f05ade2c12da61562c6c1a5
Kim JongWook <frankkim1108@naver.com>
2019-10-09 20:54:43 +0900

    10/09

--- BOJ17484.cpp
+++ BOJ17484.cpp
@@ -7,13 +7,14 @@ using namespace std;
 
 int main()
 {
-	int count = 1;
+	int count = 0;
 	int space[6][6];
 	for (int i = 0; i < 6; i++)
 		for (int j = 0; j < 6; j++) {
 			space[i][j] = 101;
 		}
-	queue <pair<int, int> > oil;
+	queue < pair< pair<int, int>, int> > oil;
+	queue < int > oilprice;
 	int N, M;
 	cin >> N >> M;
 
@@ -24,17 +25,50 @@ int main()
 	}
 
 	for (int i = 0; i < M; i++) {
-		oil.push({ space[0][i], 0 });
+		oil.push({ {0 , i}, 0 });
+		oilprice.push(space[0][i]);
 	}
 
 	while (!oil.empty()) {
+
 		int size = oil.size();
-
+		
+		if (count == N - 1)
+			break;
 		for (int i = 0; i < size; i++) {
-			int top = oil.front().first;
+			int top = oilprice.front();
 			int path = oil.front().second;
-
-			if(space)
+			int r = oil.front().first.first;
+			int c = oil.front().first.second;
+			oil.pop();
+			oilprice.pop();
+			if (r + 1 < N && c - 1 >= 0 && path != 1)
+			{
+				oil.push({ {r + 1, c - 1}, 1 });
+				oilprice.push(space[r + 1][ c - 1] + top);
+				
+			}
+			if (r + 1 < N && path != 2)
+			{
+				oil.push({ { r + 1, c}, 2 });
+				oilprice.push(space[r + 1][c] + top);
+			}
+			if (r + 1 < N && c + 1 < M && path != 3) {
+				oil.push({ { r + 1, c + 1}, 3 });
+				oilprice.push(space[r + 1][c + 1] +top);
+			}
 		}
+		count++;
 	}
+
+	int size = oilprice.front();
+	int min = 987654321;
+	while (!oilprice.empty()) {
+		if (min > oilprice.front()) {
+			min = oilprice.front();
+			
+		}
+		oilprice.pop();
+	}
+	cout << min;
 }
