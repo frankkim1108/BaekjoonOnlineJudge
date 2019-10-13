1cc16c5dbbefe0ec860dbe2f6fc0b6a801afb5ba
Kim JongWook <frankkim1108@naver.com>
2019-10-13 19:05:53 +0900

    10/13

--- BOJ17485.cpp
+++ BOJ17485.cpp
@@ -2,55 +2,44 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
-	pair<int, int> space[1000][1002];
-	for (int i = 0; i < 1000; i++) {
+int space[1002][1002][4];
+
+int main()
+{
+	for (int i = 0; i < 1002; i++) {
 		for (int j = 0; j < 1002; j++) {
-			space[i][j] = { 101, 0 };
+			space[i][j][0] = space[i][j][1] = space[i][j][2] = space[i][j][3] = 987654321;
 		}
 	}
 	int r, c; cin >> r >> c;
-	for (int i = 0; i < r; i++) {
+
+	for (int i = 1; i <= r; i++)
+	{
 		for (int j = 1; j <= c; j++) {
-			int num; cin >> num;
-			space[i][j] = { num, 0 };
+			int num;
+			cin >> num;
+			space[i][j][3] = num;
+			space[i][j][1] = num;
+			space[i][j][2] = num;
+			space[i][j][0] = num;
 		}
 	}
 
-	for (int i = 1; i < r; i++) {
+	for (int i = r-1; i >= 1; i--) {
 		for (int j = 1; j <= c; j++) {
-			int path = 0;
-			pair<int, int> current = space[i][j];
-			pair<int, int> n1 = space[i - 1][j - 1];
-			pair<int, int> n2 = space[i - 1][j];
-			pair<int, int> n3 = space[i - 1][j + 1];
-
-			
-			int num1 = 101, num2 = 101, num3 = 101;
-			if (n1.second != 3) {
-				num1 = n1.first;
-			}
-			if (n2.second != 2) {
-				num2 = n2.first;
-			}
-			if (n3.second != 1) {
-				num3 = n3.first;
-			}
-			int smallest = min(min(num1,num2),num3);
-			if (smallest == num1)
-				path = 3;
-			else if (smallest == num2)
-				path = 2;
-			else
-				path = 1;
-
-			space[i][j] = { current.first + smallest, path };
+			int num3 = min(space[i + 1][j - 1][1], space[i + 1][j - 1][2]);
+			int num2 = min(space[i + 1][j][1], space[i + 1][j][3]);
+			int num1 = min(space[i + 1][j + 1][3], space[i + 1][j + 1][2]);
+				space[i][j][1] += num1;
+				space[i][j][2] += num2;
+				space[i][j][3] += num3;
 		}
 	}
-
+	int small = 987654321;
 	for (int i = 1; i <= c; i++) {
-		cout << space[r-1][i].first << " ";
+		if (small > min(min(space[1][i][1], space[1][i][2]), space[1][i][3]))
+			small = min(min(space[1][i][1], space[1][i][2]), space[1][i][3]);
 	}
 
-	return 0;
+	cout << small;
 }
